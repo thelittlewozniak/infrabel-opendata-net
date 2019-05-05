d4dadd24ef95675cc0babfe985afc3c05d5a66e0
Nathan Pire <nathan.pire@gmail.com>
2019-05-05 17:39:28 +0200

    finishing security part and documentation done for this part

---
+++ OpenDataInfrabel/Model/Security/AutomaticProtectionSystem/Record.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Security.AutomaticProtectionSystem
+{
+    public class Record
+    {
+        public string Datasetid { get; set; }
+        public string Recordid { get; set; }
+        public Fields Fields { get; set; }
+        public DateTime Record_timestamp { get; set; }
+    }
+}
--- OpenDataInfrabel/TestProgram/Program.cs
+++ OpenDataInfrabel/TestProgram/Program.cs
@@ -12,7 +12,10 @@ namespace TestProgram
             var result2 = await security.GetAccidentsISI();
             var result3 = await security.GetViolationSignalRailwayPrimary();
             var result4 = await security.GetViolationSignalRailwaySecondary();
-            Console.WriteLine(result + "" +result2 + "" + result3 + "" + result4);
+            var result5 = await security.GetViolationSignalRailwayPrimaryPerTrain();
+            var result6 = await security.GetViolationRailway();
+            var result7 = await security.GetPrecursorsSecurity();
+            Console.WriteLine(result + "" +result2 + "" + result3 + "" + result4 + "" + result5 + "" + result6 + "" + result7);
         }
     }
 }

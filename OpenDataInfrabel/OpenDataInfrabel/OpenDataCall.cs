cf3dacbf98f12f42ec71e5437757c6ace6f6f951
HunteRoi <32441291+HunteRoi@users.noreply.github.com>
2019-06-02 22:10:42 +0200

    Use StringBuilder instead of string concatenation

--- OpenDataInfrabel/OpenDataInfrabel/OpenDataCall.cs
+++ OpenDataInfrabel/OpenDataInfrabel/OpenDataCall.cs
@@ -1,34 +1,45 @@
-﻿using System;
+using System;
+using System.Text;
 using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace OpenDataInfrabel
 {
-    public class OpenDataCall:IDisposable
+    public class OpenDataCall : IDisposable
     {
+        private static readonly string url = "https://opendata.infrabel.be/api/records/1.0/search/?";
         private readonly HttpClient httpClient;
         private bool disposed = false;
-        private static readonly string url = "https://opendata.infrabel.be/api/records/1.0/search/?";
-        public OpenDataCall() => httpClient = new HttpClient();
+        
+        public OpenDataCall()
+        {
+            httpClient = new HttpClient();
+        }
+        
         protected async Task<string> MakeCall(string dataset, string q, string lang, int rows, int start, string[] facet)
         {
-            var finalUrl = url + "dataset=" + dataset;
+            StringBuilder finalUrl = new StringBuilder($"{url}dataset={dataset}");
             for (int i = 0; i < facet.Length; i++)
             {
-                finalUrl += ("&facet=" + facet[i]);
+                finalUrl.Append($"&facet={facet[i]}");
             }
-            finalUrl += (q == null ? "" : "&q=" + q) + "&lang=" + lang + "&rows=" + rows + "&start=" + start;
-            var request = await httpClient.GetAsync(finalUrl);
+            finalUrl.Append(q == null ? "" : $"&q={q}"));
+            finalUrl.Append($"&lang={lang}&rows={rows}&start={start}");
+
+            var request = await httpClient.GetAsync(finalUrl.ToString());
             if (request.StatusCode != System.Net.HttpStatusCode.OK)
                 return null;
+
             var requestString = await request.Content.ReadAsStringAsync();
             return requestString;
         }
+        
         public void Dispose()
         {
             Dispose(true);
             GC.SuppressFinalize(this);
         }
+        
         protected virtual void Dispose(bool disposing)
         {
             if (!disposed)
